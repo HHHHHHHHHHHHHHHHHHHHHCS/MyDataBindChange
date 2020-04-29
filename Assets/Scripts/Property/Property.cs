ee25ff0fb89e70c314251bad276d2c491d6a06f9
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-04-29 14:53:12 +0800

    Initial commit

---
+++ Assets/Scripts/Property/Property.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property<T>
+{
+    private event Action<T> actEvent;
+    private T _val;
+
+    public bool IsForces { get; set; }
+
+    public T Val
+    {
+        get => _val;
+        set
+        {
+            if (_val != null && value != null)
+            {
+                if (!_val.Equals(value))
+                {
+                    _val = value;
+                    actEvent?.Invoke(Val);
+                }
+                else if (IsForces)
+                {
+                    actEvent?.Invoke(Val);
+                }
+            }
+            else if (_val == null && value == null)
+            {
+                if (IsForces)
+                {
+                    actEvent?.Invoke(Val);
+                }
+            }
+            else
+            {
+                _val = value;
+                actEvent?.Invoke(Val);
+            }
+        }
+    }
+
+    public Property(T val = default(T), Action<T> act = null, bool _isForces = false)
+    {
+        _val = val;
+        Add(act);
+        IsForces = _isForces;
+    }
+
+    public void Add(Action<T> act)
+    {
+        if (act != null)
+        {
+            actEvent += act;
+        }
+    }
+
+    public void Remove(Action<T> act)
+    {
+        if (act != null)
+        {
+            actEvent -= act;
+        }
+    }
+
+    public void Set(Action<T> act)
+    {
+        actEvent = act;
+    }
+
+    public void Clear()
+    {
+        actEvent = null;
+    }
+
+    public static implicit operator T(Property<T> p)
+    {
+        return p._val;
+    }
+}
---
+++ Assets/Tests/PropertyTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PropertyTest
+    {
+        struct MyStruct
+        {
+            public string str;
+
+            public override string ToString()
+            {
+                return str;
+            }
+        }
+
+        class MyClass
+        {
+            public string str;
+
+            public override string ToString()
+            {
+                return str;
+            }
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PropertyTestSimplePasses()
+        {
+            Property<bool> b0 = new Property<bool>(false, (x) => { Debug.Log(x); });
+            b0.Val = true;
+            Assert.IsTrue(b0 == true);
+
+            Property<bool> b1 = new Property<bool>(false, (x) => { Debug.Log(x); }, true);
+            b1.Val = false;
+            Assert.IsTrue(b1.Val == false);
+
+            Property<MyStruct> s0 = new Property<MyStruct>(act: (x) => { Debug.Log(x); });
+            s0.Val = new MyStruct() {str = "asdasd"};
+            Assert.IsTrue(s0.Val.str == "asdasd");
+
+
+            Property<MyClass> c0 = new Property<MyClass>(act: (x) => { Debug.Log(x); });
+            var cls = new MyClass() { str = "aaaa" };
+            c0.Val = cls;
+            Assert.IsTrue(c0.Val == cls);
+
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        //        [UnityTest]
+        //        public IEnumerator PropertyTestWithEnumeratorPasses()
+        //        {
+        //            // Use the Assert class to test conditions.
+        //            // Use yield to skip a frame.
+        //            yield return null;
+        //        }
+    }
+}
